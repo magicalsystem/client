9c851b9561f0b6b6fd6cb47a1f0ddb474a3fdbf4
Jakub Woźniak <mescam@mescam.pl>
2015-11-29 13:24:12 +0100

    keys commands

--- akane/cmd_user_keys.py
+++ akane/cmd_user_keys.py
@@ -12,3 +12,21 @@ def add(username, pubkey):
     pk = pubkey.read()  # read public key from file or stdin
     # do sth with pk
     click.secho("Added key to %s" % username, fg='green')
+
+@key.command("del")
+@click.argument("username")
+@click.argument("number", type=int)
+def delete(username, number):
+    click.secho("Deleted key from {username}".format(username=username),
+                fg='green')
+
+@key.command()
+@click.argument("username")
+def list(username):
+    pass
+
+@key.command()
+@click.argument("username")
+@click.argument("number", type=int)
+def show(username, number):
+    pass
